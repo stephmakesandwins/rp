390f529d697413007b9310b9ceb480487706718a
stephmakesandwins <144512533+stephmakesandwins@users.noreply.github.com>
2023-12-28 16:57:55 -0500

    Add files via upload

---
+++ slick-rp.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.rp-slider').slick({
+        slide: '.rp-slide',
+      autoplay: true,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplaySpeed: 3000,
+      }
+    });
+  });
